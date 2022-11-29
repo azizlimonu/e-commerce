435d1e61e85ab5ec8a41f18a529e1bf87421958e
Aziz Limonu <azizlimonu@gmail.com>
2022-11-29 10:54:53 +0800

    change and fix api request

---
+++ frontend/src/apiRequest.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiRequest = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+  },
+});
--- frontend/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import './FeaturedProduct.scss';
+import axios from 'axios';
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Shirt",
-      oldPrice: "19",
-      price: "12",
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Shirt",
-      oldPrice: "19",
-      price: "12",
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Shirt",
-      oldPrice: "19",
-      price: "12",
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Shirt",
-      oldPrice: "19",
-      price: "12",
-    },
-  ]
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(process.env.REACT_APP_API_URL+`/products?populate=*&[filters][type][$eq]=${type}`, {
+        headers:{
+          Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+        }
+        });
+        console.log(res);
+        setData(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [])
+
+
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Shirt",
+  //     oldPrice: "19",
+  //     price: "12",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Shirt",
+  //     oldPrice: "19",
+  //     price: "12",
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Shirt",
+  //     oldPrice: "19",
+  //     price: "12",
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Shirt",
+  //     oldPrice: "19",
+  //     price: "12",
+  //   },
+  // ]
 
   return (
     <div className='featuredProducts'>
@@ -46,7 +66,7 @@ const FeaturedProducts = ({ type }) => {
           suspendisse ultrices gravida. Risus commodo viverra maecenas.
         </p>
       </div>
-      <div className='bottom'>
+      <div className='bottom'> 
         {data?.map((item) => (
           <Card item={item} key={item.id} />
         ))}
